a8ecdc924ae053a808346705bc1e335c1182bb30
janghun93 <pain3992@gmail.com>
2024-05-13 09:17:31 +0900

    사칙연산 추가

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a;
+	float b;
+
+	cin >> a >> b;
+
+	cout << "A + B = " << a + b << endl;
+	cout << "A - B = " << a - b << endl;
+	cout << "A * B = " << a * b << endl;
+	cout << "A / B = " << a / b << endl;
+}
